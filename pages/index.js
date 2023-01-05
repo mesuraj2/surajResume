a3f75d8e894767f4c35e9149c20e7235a7b5dbb0
Suraj Kumar <znsuraj7@gmail.com>
2023-01-05 13:51:50 +0530

    changed titile

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Suraj kumar</title>
         <meta name="description" content="Suraj's resume" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
